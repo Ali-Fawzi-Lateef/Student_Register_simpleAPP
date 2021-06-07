759b5c8c3f754867e010ca3a39ba9731ff61ef5c
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-06-07 15:57:26 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ public:
 		std::cout << "Enter student name\n";
 		std::cin >> studentName;//get input from the user
 		std::cout << "Enter student ID\n";
-		std::cin >> studentID;
+		std::cin >> studentID;//get input from the user
 		std::cout << "Enter student rateing\n";
 		std::cin >> rateing;
 	}
