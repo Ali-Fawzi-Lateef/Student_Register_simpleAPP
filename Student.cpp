e30187758be38a953beb25f5f69cc94fb6222ef5
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-07-01 16:19:33 +0300

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -10,7 +10,7 @@ public:
 		std::cin >> studentName;//get input from the user.
 		std::cout << "Enter student ID\n";//print the text to the console.
 		std::cin >> studentID;//get input from the user.
-		std::cout << "Enter student rateing\n";//print the text to the console
+		std::cout << "Enter student rateing\n";//print the text to the console.
 		std::cin >> rateing;//get input from the user.
 		std::cout << "\a";
 	}
