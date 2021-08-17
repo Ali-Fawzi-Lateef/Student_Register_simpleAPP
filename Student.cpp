3787a575a3361659ed4d2e15d3dcddf2f35ca1b6
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-08-17 19:17:56 +0300

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -6,7 +6,7 @@ public:
 	int studentID;
 	float rateing;
 	void Mystudent() {
-		std::cout << "Enter student name\n";//print the text to the console
+		std::cout << "Enter student name\n";//print the text to the console.
 		std::cin >> studentName;//get input from the user
 		std::cout << "Enter student ID\n";//print the text to the console.
 		std::cin >> studentID;//get input from the user.
